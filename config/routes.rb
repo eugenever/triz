eed84148761e9bfd5be22f2fd37facbe05662f5c
eugenever <eugenepr@yandex.ru>
2014-04-18 18:43:01 +0400

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TriZ::Application.routes.draw do
+  get "static_pages/home"
   mount Ckeditor::Engine => '/ckeditor'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
