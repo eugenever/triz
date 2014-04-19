cbb702dba0bf7506d979f54e7704d81c05fe53a2
eugenever <eugenepr@yandex.ru>
2014-04-19 19:27:32 +0400

    maket 1-st page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,1 +1,16 @@
+$(window).resize(function() {
+    
+    //изменяем отступ body при расширении навбара
+    var navbar_height = $('#nbheader').height();
+    $('body').css('padding-top', navbar_height + 'px');       
+ 
+});
 
+
+$(document).ready(function() { 
+    
+  //изменяем отступ body при расширении навбара
+  var navbar_height = $('#nbheader').height();
+  $('body').css('padding-top', navbar_height + 'px');
+
+});
