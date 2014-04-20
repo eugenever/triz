0417dbe7fb8df3682de8346ca945f95f0d36a373
eugenever <eugenepr@yandex.ru>
2014-04-20 22:07:22 +0400

    developing layout

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,16 +1,12 @@
-$(window).resize(function() {
-    
+$(window).resize(function() {    
     //изменяем отступ body при расширении навбара
     var navbar_height = $('#nbheader').height();
-    $('body').css('padding-top', navbar_height + 'px');       
- 
+    $('body').css('padding-top', navbar_height + 'px'); 
 });
 
 
-$(document).ready(function() { 
-    
+$(document).ready(function() {    
   //изменяем отступ body при расширении навбара
   var navbar_height = $('#nbheader').height();
-  $('body').css('padding-top', navbar_height + 'px');
-
+  $('body').css('padding-top', navbar_height + 'px');  
 });
