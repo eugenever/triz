75e29f851c871dd9520c0f58d4b12098f9f25932
eugenever <eugenepr@yandex.ru>
2014-04-22 18:43:34 +0400

    develop layout 6

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,22 @@
+//кнопка вверх
+$(function() { 
+  $.fn.scrollToTop = function() { 
+    
+    $(this).hide().removeAttr("href"); if ($(window).scrollTop() != "0") { 
+      $(this).fadeIn("slow")
+    }
+    
+    var scrollDiv = $(this); $(window).scroll(function() {
+      if ($(window).scrollTop() == "0") { $(scrollDiv).fadeOut("slow") }
+      else { $(scrollDiv).fadeIn("slow") }
+    });
+
+    $(this).click(function() { $("html, body").animate({ scrollTop: 0 }, "slow") })
+  }
+});
+
+$(function() { $("#w2b-StoTop").scrollToTop(); });
+
 $(window).resize(function() {    
     //изменяем отступ body при расширении навбара
     var navbar_height = $('#nbheader').height();
@@ -8,5 +27,10 @@ $(window).resize(function() {
 $(document).ready(function() {    
   //изменяем отступ body при расширении навбара
   var navbar_height = $('#nbheader').height();
-  $('body').css('padding-top', navbar_height + 'px');  
+  $('body').css('padding-top', navbar_height + 'px');
 });
+
+
+
+
+
